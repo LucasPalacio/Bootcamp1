459594ee1e0befbc6c72612dee493a8090c553a2
Lucas Palacio <lucaspalaciomelo@gmail.com>
2025-05-09 18:11:43 -0300

    primeiro codigo

---
+++ Exercicio1ParImparController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace algoritmoOperadoresTernarios.Controllers
+{
+    public class Exercicio1ParImparController : ControllerBase
+    {
+        [HttpPost("VerificarNumeroParImpar")]
+        public String VerificarNumeroParImpar(Int32 numero)
+        {
+            string resultado;
+            //if (numero % 2 == 0)
+            //{
+            //    resultado = "é Par";
+            //}
+            //else {
+            //    resultado = "é impar";
+            //}
+            //usando operadores ternários
+            resultado = (numero % 2 == 0) ?"Par" : "Impar";
+            return $"O {numero} é {resultado}";
+        }
+    }
+}
